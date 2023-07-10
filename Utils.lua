b9f94266b423eee24e7907fd024618954fa9470b
enderneko <enderneko-dev@outlook.com>
2023-07-10 16:17:44 +0800

    Update text filter

--- Utils.lua
+++ Utils.lua
@@ -21,25 +21,65 @@ function MRT_NL:SendChatMessage(lines)
     -- remove color codes
     lines = string.gsub(lines, "\124\124c%w%w%w%w%w%w%w%w", "")
     lines = string.gsub(lines, "\124\124r", "")
+    
+    -- {time:mm:ss}, {time:ss}
+    lines = string.gsub(lines, "%{time:%d+:%d+%}", function(text)
+        local m, s = strmatch(text, "(%d+):(%d+)")
+        m = tonumber(m)
+        s = format("%02d", s)
+        return "["..m..":"..s.."]"
+    end)
+    lines = string.gsub(lines, "%{time:%d+%}", function(text)
+        local s = tonumber(strmatch(text, "%d+"))
+        s = format("%02d", s)
+        return "[0:"..s.."]"
+    end)
+
+    -- {time:mm:ss,event:spellId:count}, {time:ss,event:spellId:count}
+    lines = string.gsub(lines, "%{time:%d+:%d+,%a+:%d+:%d+%}", function(text)
+        local m, s, id, count = strmatch(text, "(%d+):(%d+),%a+:(%d+):(%d+)")
+        m = tonumber(m)
+        s = format("%02d", s)
+
+        count = tonumber(count)
+        if count == 0 then count = "" end
+
+        local link = GetSpellLink(id) or ""
+
+        return count..link.."["..m..":"..s.."]"
+    end)
+    lines = string.gsub(lines, "%{time:%d+,%a+:%d+:%d+%}", function(text)
+        local s, id, count = strmatch(text, "(%d+),%a+:(%d+):(%d+)")
+        s = format("%02d", s)
+
+        count = tonumber(count)
+        if count == 0 then count = "" end
+
+        local link = GetSpellLink(id) or ""
+        
+        return count..link.."[0:"..s.."]"
+    end)
+
+
+    -- {spell:xxx} -> link
+    lines = string.gsub(lines, "%{spell:%d+%}", function(text)
+        local id = tonumber(strmatch(text, "%d+"))
+        local link = GetSpellLink(id)
+        if link then
+            return link
+        else
+            return ""
+        end
+    end)
 
     -- convert to string[]
     lines = strsplittable("\n", lines)
 
-    -- {spell:xxx} -> link
-    for i = 1, #lines do
-        lines[i] = string.gsub(lines[i], "%{spell:%d+%}", function(text)
-            local id = tonumber(strmatch(text, "%d+"))
-            local link = GetSpellLink(id)
-            if link then
-                return link
-            else
-                return ""
-            end
-        end)
-    end
-
     -- test
     -- texplore(lines)
+    -- for _, line in pairs(lines) do
+    --     print(line)
+    -- end
     -- C_Timer.After(1, function()
     --     isWaitForSending = false
     --     MRT_NL_Send:SetEnabled(true)
